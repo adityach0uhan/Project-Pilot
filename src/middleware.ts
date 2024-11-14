06c0ff63b9fa9ccdc8206694a117a43ecd5a7472
adityach0uhan <m4with6x@gmail.com>
2024-11-14 23:25:55 +0530

    added more svgs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,49 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 import jwt from 'jsonwebtoken';
-// import { verifyToken } from './helpers/verifyToken';
+import { cookies } from 'next/headers';
 
 export async function middleware(request: NextRequest) {
-    //     const token = await request.cookies.get('student_project_manager_token')
-    //         ?.value;
-    //     const decodedToken = await verifyToken();
-    //     const currentPath = request.nextUrl.pathname;
-    //     const protectedPaths = currentPath.includes('/dashboard');
-    //     const hidePathForAlreadyLoggedInUser = currentPath.includes('/auth');
-    //     // Redirect to login page if token is not present
-    //     if (!token && protectedPaths) {
-    //         return NextResponse.redirect(new URL('/auth/login', request.url));
-    //     }
-    //     // Redirect to dashboard according to role
-    //     if (token && protectedPaths) {
-    //         if (decodedToken.role === 'teacher') {
-    //             return NextResponse.redirect(
-    //                 new URL('/dashboard/teacher', request.url)
-    //             );
-    //         } else if (decodedToken.role === 'student') {
-    //             return NextResponse.redirect(
-    //                 new URL('/dashboard/student', request.url)
-    //             );
-    //         }
-    //     }
-    //     // Hide path for already logged in user
-    //     if (hidePathForAlreadyLoggedInUser) {
-    //         if (token) {
-    //             if (decodedToken.role === 'teacher') {
-    //                 return NextResponse.redirect(
-    //                     new URL('/dashboard/teacher', request.url)
-    //                 );
-    //             } else if (decodedToken.role === 'student') {
-    //                 return NextResponse.redirect(
-    //                     new URL('/dashboard/student', request.url)
-    //                 );
-    //             }
-    //         }
-    //     }
+    const token = await request.cookies.get('student_project_manager_token')
+        ?.value;
+    const decodeToken = (await jwt.decode(token!)) as { role?: string };
+    const currentPath = request.nextUrl.pathname;
+    const protectedPaths = currentPath.includes('/dashboard');
+
+    if (protectedPaths) {
+        if (!token) {
+            return NextResponse.redirect(new URL('/auth/login', request.url));
+        } else {
+            if (decodeToken.role === 'teacher') {
+                if (currentPath.includes('/student')) {
+                    return NextResponse.redirect(
+                        new URL('/dashboard/teacher', request.url)
+                    );
+                }
+            } else if (decodeToken.role === 'student') {
+                if (currentPath.includes('/teacher')) {
+                    return NextResponse.redirect(
+                        new URL('/dashboard/student', request.url)
+                    );
+                }
+            }
+        }
+    }
+
+    const hidePathForAlreadyLoggedInUser = currentPath.includes('/auth');
+    if (hidePathForAlreadyLoggedInUser) {
+        if (token) {
+            if (decodeToken.role === 'teacher') {
+                return NextResponse.redirect(
+                    new URL('/dashboard/teacher', request.url)
+                );
+            } else if (decodeToken.role === 'student') {
+                return NextResponse.redirect(
+                    new URL('/dashboard/student', request.url)
+                );
+            }
+        }
+    }
 }
 
 export const config = {

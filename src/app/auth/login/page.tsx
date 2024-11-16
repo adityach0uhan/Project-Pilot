52886822ad8837ec2b24c5252f9b0da3c02cfd52
adityach0uhan <m4with6x@gmail.com>
2024-11-16 20:08:09 +0530

    added teacher code validation

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -17,6 +17,7 @@ import axios, { AxiosResponse } from 'axios';
 import { InputOTP, InputOTPSlot } from '@/components/ui/input-otp';
 import Link from 'next/link';
 import Image from 'next/image';
+import { toast } from 'sonner';
 
 export default function page() {
     const router = useRouter();
@@ -26,6 +27,7 @@ export default function page() {
     const [teacherPassword, setTeacherPassword] = useState('');
     const [loading, setLoading] = useState(false);
     const [teacherSecret, setTeacherSecret] = useState('');
+    const [secretError, setSecretError] = useState('');
 
     const handleStudentLogin: any = async (): Promise<void> => {
         try {
@@ -40,15 +42,23 @@ export default function page() {
                 { withCredentials: true }
             );
 
-            console.log('Login successful:', response.data);
             if (response.data.success && response.data.status == 200) {
+                toast.success('Login successful', {
+                    position: 'bottom-center',
+                    duration: 2000
+                });
                 router.push('/dashboard/student');
             }
+            console.log('Login successful:', response.data);
             setLoading(false);
-
             setstudentPassword('');
             setStudentEmail('');
         } catch (error: any) {
+            toast.error('Login failed', {
+                description: 'Invalid email or password',
+                position: 'bottom-center',
+                duration: 3000
+            });
             setLoading(false);
             setstudentPassword('');
             setStudentEmail('');
@@ -65,10 +75,17 @@ export default function page() {
 
     const handelTeacherLogin: any = async (): Promise<void> => {
         const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
-        const teacherSecret = process.env.NEXT_PUBLIC_TEACHER_SECRET;
-        console.log('API endpoint:', apiEndpoint);
+        const teacherSecretCode = process.env.NEXT_PUBLIC_TEACHER_SECRET;
         try {
             setLoading(true);
+            if (teacherSecret != teacherSecretCode) {
+                toast.error('Invalid Secret Code');
+                setSecretError('Wrong Secret Code');
+                setTeacherSecret('');
+                setLoading(false);
+                return;
+            }
+            setSecretError('Right Secret Code');
             const response = await axios.post(
                 `${apiEndpoint}/auth/teacher/login`,
                 {
@@ -77,17 +94,26 @@ export default function page() {
                 },
                 { withCredentials: true }
             );
-            setLoading(false);
-            console.log('Teacher login successful:', response.data);
-            if (response.data.success && response.data.status === 200) {
+            console.log(response);
+            if (response.data.success && response.data.status == 200) {
+                toast.success('Login successful', {
+                    position: 'bottom-center',
+                    duration: 2000
+                });
                 router.push('/dashboard/teacher');
             }
+            setLoading(false);
             setTeacherEmail('');
             setTeacherPassword('');
         } catch (error: any) {
             setLoading(false);
             setTeacherEmail('');
             setTeacherPassword('');
+            toast.error('Login failed', {
+                description: 'Invalid email or password',
+                position: 'bottom-center',
+                duration: 3000
+            });
             if (axios.isAxiosError(error)) {
                 console.log(
                     'Error with the request:',
@@ -118,13 +144,13 @@ export default function page() {
                 />
                 <Tabs
                     defaultValue='student'
-                    className='w-[350px] my-auto mx-2   md:mt-20'>
+                    className='w-[350px] my-auto mx-2 '>
                     <TabsList className='grid w-full grid-cols-2 '>
                         <TabsTrigger value='student'>Student </TabsTrigger>
                         <TabsTrigger value='teacher'>Teacher </TabsTrigger>
                     </TabsList>
                     <TabsContent value='student'>
-                        <Card className='h-80  '>
+                        <Card className='min-h-80  '>
                             <CardHeader>
                                 <CardTitle>Student Login</CardTitle>
                                 <CardDescription>
@@ -158,7 +184,7 @@ export default function page() {
                                 <Link
                                     className='text-sm mt-1 text-blue-600 underline underline-offset-4 '
                                     href={'/forgot-password'}>
-                                    frogot password ?
+                                    forgot password ?
                                 </Link>
                             </CardContent>
                             <CardFooter>
@@ -173,22 +199,58 @@ export default function page() {
                         </Card>
                     </TabsContent>
                     <TabsContent value='teacher'>
-                        <Card className='h-80'>
-                            <CardHeader>
-                                <CardTitle>Teacher Login</CardTitle>
-                                <CardDescription>
-                                    Enter the teacher Secret Code
-                                    <InputOTP maxLength={6}>
-                                        <InputOTPSlot index={0} />
-                                        <InputOTPSlot index={1} />
-                                        <InputOTPSlot index={2} />
-                                        <InputOTPSlot index={3} />
-                                        <InputOTPSlot index={4} />
-                                        <InputOTPSlot index={5} />
-                                    </InputOTP>
-                                </CardDescription>
-                            </CardHeader>
+                        <Card className='min-h-80'>
+                            <CardTitle className='px-6 pt-6 pb-3'>
+                                Teacher Login
+                            </CardTitle>
                             <CardContent className=''>
+                                <div className='flex items-center justify-between  my-1'>
+                                    <p className='text-sm'>
+                                        Teacher Secret Code
+                                    </p>
+                                    {secretError && (
+                                        <span className='text-xs text-red-500 gap-1 flex items-center '>
+                                            <Image
+                                                src={'/wrong.svg'}
+                                                alt='Error'
+                                                width={15}
+                                                height={15}
+                                            />
+                                            {secretError}
+                                        </span>
+                                    )}
+                                </div>
+                                <InputOTP
+                                    maxLength={6}
+                                    value={teacherSecret}
+                                    onChange={(value) => {
+                                        setTeacherSecret(value);
+                                    }}>
+                                    <InputOTPSlot
+                                        className='rounded-md '
+                                        index={0}
+                                    />
+                                    <InputOTPSlot
+                                        className='rounded-md '
+                                        index={1}
+                                    />
+                                    <InputOTPSlot
+                                        className='rounded-md '
+                                        index={2}
+                                    />
+                                    <InputOTPSlot
+                                        className='rounded-md '
+                                        index={3}
+                                    />
+                                    <InputOTPSlot
+                                        className='rounded-md '
+                                        index={4}
+                                    />
+                                    <InputOTPSlot
+                                        className='rounded-md '
+                                        index={5}
+                                    />
+                                </InputOTP>
                                 <div className=''>
                                     <Label htmlFor='teacher-email'>Email</Label>
                                     <Input

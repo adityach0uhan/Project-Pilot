57294a4acb13c731bf4c3fc690e36d25af10d6fc
adityach0uhan <m4with6x@gmail.com>
2025-01-31 22:27:53 +0530

    college card for each college

---
+++ src/components/super-admin/CollegeCard.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Separator } from '@/components/ui/separator';
+import { Pencil1Icon, TrashIcon } from '@radix-ui/react-icons';
+export function CollegeCard({ collegeData }: any) {
+    return (
+        <Card className='w-[350px]'>
+            <CardHeader>
+                <CardTitle className='flex justify-between'>
+                    <span>{collegeData.collegeName}</span>
+                    <span>{collegeData.collegeId}</span>
+                </CardTitle>
+                <CardDescription>{collegeData.collegeLocation}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className='w-full flex justify-between'>
+                    Email <span>{collegeData.email}</span>
+                </div>
+                <Separator className='my-2' />
+                <div className='w-full flex justify-between'>
+                    Passkey<span>{collegeData.passkey}</span>
+                </div>
+            </CardContent>
+            <CardFooter className='flex justify-between'>
+                <Button variant='outline'>
+                    Delete
+                    <TrashIcon className='w-6 h-6 text-red-500 ' />
+                </Button>
+                <Button variant='outline'>
+                    Update
+                    <Pencil1Icon className='w-6 h-6 text-yellow-500' />
+                </Button>
+            </CardFooter>
+        </Card>
+    );
+}

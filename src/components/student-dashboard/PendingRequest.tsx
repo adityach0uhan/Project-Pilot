556daffb25f285af65a68f7c5e1f52488627da94
adityach0uhan <m4with6x@gmail.com>
2025-01-31 22:27:00 +0530

    added pending request tab

--- src/components/student-dashboard/PendingRequest.tsx
+++ src/components/student-dashboard/PendingRequest.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Button } from '@/components/ui/button';
 import {
     Dialog,
@@ -7,11 +8,47 @@ import {
     DialogTrigger
 } from '@/components/ui/dialog';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-
+import { CheckIcon, Cross1Icon, ReloadIcon } from '@radix-ui/react-icons';
+import axios from 'axios';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/lib/store';
+import { get } from 'http';
+import { set } from 'zod';
 interface PendingRequestProps {
     groupData: any;
+    getTeamInfo: any;
 }
-const PendingRequest: React.FC<PendingRequestProps> = ({ groupData }) => {
+
+const PendingRequest: React.FC<PendingRequestProps> = ({
+    groupData,
+    getTeamInfo
+}) => {
+    const user = useSelector((state: RootState) => state.user);
+    const [loading, setLoading] = useState(false);
+    const [responseFromServer, setResponseFromServer] = useState<any>(null);
+    const RequestFunction = async (action: string, userId: string) => {
+        try {
+            setLoading(true);
+            const resp = await axios.post(
+                'http://localhost:4000/group/managejoinrequests',
+                {
+                    userId: userId,
+                    action: action,
+                    currentUserId: user._id,
+                    groupID: groupData._id
+                }
+            );
+            setResponseFromServer(resp.data);
+        } catch (error) {
+            console.log('Error', error);
+        } finally {
+            getTeamInfo();
+            setLoading(false);
+            console.log('Finally');
+        }
+    };
+
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -41,10 +78,33 @@ const PendingRequest: React.FC<PendingRequestProps> = ({ groupData }) => {
                             <p>{item.name}</p>
                             <p className='text-xs'>{item.email}</p>
                         </div>
-                        <Button className='max-w-20 h-8'>Accept</Button>
-                        <Button className='max-w-20 h-8'>Reject</Button>
+                        <div className='flex gap-2'>
+                            {loading ? (
+                                <Button>
+                                    <ReloadIcon className='animate-spin' />
+                                </Button>
+                            ) : (
+                                <>
+                                    <Button
+                                        onClick={() =>
+                                            RequestFunction('accept', item._id)
+                                        }
+                                        className='max-w-20 h-8'>
+                                        <CheckIcon />
+                                    </Button>
+                                    <Button
+                                        className='max-w-20 h-8'
+                                        onClick={() =>
+                                            RequestFunction('reject', item._id)
+                                        }>
+                                        <Cross1Icon />
+                                    </Button>
+                                </>
+                            )}
+                        </div>
                     </div>
                 ))}
+                {responseFromServer && <div>{responseFromServer.message}</div>}
             </DialogContent>
         </Dialog>
     );

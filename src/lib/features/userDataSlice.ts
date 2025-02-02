15bcf6a830840a03b1120772e6be71daaa4402a5
adityach0uhan <m4with6x@gmail.com>
2025-02-02 21:40:37 +0530

    changes in slice

--- src/lib/features/userDataSlice.ts
+++ src/lib/features/userDataSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
-export interface userSlice {
+export interface UserState {
     _id: string;
     name: string;
     email: string;
@@ -18,9 +18,11 @@ export interface userSlice {
     teacherId?: string;
     designation?: string;
     isHOD?: boolean;
+    groupNumber?: string;
     collegeName?: string;
 }
-const initialState: userSlice = {
+
+const initialState: UserState = {
     _id: '',
     name: '',
     email: '',
@@ -37,34 +39,21 @@ const initialState: userSlice = {
     designation: '',
     teacherId: '',
     collegeName: '',
-    collegeLocation: ''
+    collegeLocation: '',
+    groupNumber: ''
 };
 
 export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        addCurrentUserData: (state, action: PayloadAction<userSlice>) => {
-            state._id = action.payload._id;
-            state.name = action.payload.name;
-            state.email = action.payload.email;
-            state.profilePic = action.payload.profilePic;
-            state.department = action.payload.department;
-            state.semester = action.payload.semester;
-            state.role = action.payload.role;
-            state.teacherId = action.payload.teacherId;
-            state.designation = action.payload.designation;
-            state.gender = action.payload.gender;
-            state.isHOD = action.payload.isHOD;
-            state.section = action.payload.section;
-            state.classRollNumber = action.payload.classRollNumber;
-            state.collegeId = action.payload.collegeId;
-            state.branch = action.payload.branch;
-            state.collegeName = action.payload.collegeName;
+        addCurrentUserData: (
+            state,
+            action: PayloadAction<Partial<UserState>>
+        ) => {
+            return { ...state, ...action.payload };
         },
-        logout: (state) => {
-            return initialState;
-        }
+        logout: () => initialState
     }
 });
 
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -13,7 +13,6 @@ const rootReducer = combineReducers({
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
     reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
